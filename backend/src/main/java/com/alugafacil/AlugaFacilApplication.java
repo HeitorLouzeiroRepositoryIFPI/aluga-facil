646746448a740a45adc5c339eb23a511fa5fce39
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-02-06 20:05:26 -0300

    feat(backend): adiciona classe principal da aplicação

---
+++ backend/src/main/java/com/alugafacil/AlugaFacilApplication.java
@@ -0,0 +1,11 @@
+package com.alugafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaFacilApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlugaFacilApplication.class, args);
+    }
+}
