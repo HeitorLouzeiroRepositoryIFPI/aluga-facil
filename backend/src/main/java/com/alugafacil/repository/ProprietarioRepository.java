608b784f015957f502ac5f0cc7f794b706006d40
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-02-07 22:46:09 -0300

    feat: removendo proprietario.

--- backend/src/main/java/com/alugafacil/repository/ProprietarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alugafacil.repository;
-
-import com.alugafacil.model.Proprietario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
-}
