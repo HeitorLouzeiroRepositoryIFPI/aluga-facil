16c7fa8159b596bf9f5400f5eaf705230c7da23d
Heitor Louzeiro <heiorlouzeiro2019@gmail.com>
2025-02-13 00:40:39 -0300

    fix: Resolvendo erro do react do id.

--- frontend/src/app/admin/dashboard/clientes/editar/[id]/page.tsx
+++ frontend/src/app/admin/dashboard/clientes/editar/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useAuth } from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
+import { use } from "react";
 import EditarClienteForm from "./components/EditarClienteForm";
 
 interface EditarClientePageProps {
@@ -13,6 +14,7 @@ interface EditarClientePageProps {
 export default function EditarClientePage({ params }: EditarClientePageProps) {
   const router = useRouter();
   const { user, userType } = useAuth();
+  const { id } = use(params);
 
   if (!user || userType !== 'admin') {
     router.push('/login');
@@ -20,11 +22,9 @@ export default function EditarClientePage({ params }: EditarClientePageProps) {
   }
 
   return (
-    <div className="container mx-auto py-10">
-      <div className="mb-6">
-        <h1 className="text-2xl font-bold">Editar Cliente</h1>
-      </div>
-      <EditarClienteForm id={parseInt(params.id)} />
+    <div className="container mx-auto py-6">
+      <h1 className="text-2xl font-bold mb-6">Editar Cliente</h1>
+      <EditarClienteForm id={parseInt(id)} />
     </div>
   );
 }
